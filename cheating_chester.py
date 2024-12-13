f9ea72a4d139386dfb5649bd91ca6d96286bad2e
ethanostic <ethanclausing@gmail.com>
2024-12-13 11:52:32 +0300

    Upgaded Cheating Chester
    Cheating Chester still does not work as he cannot import the main rps file without causing a loop. The only solution i can think of is to import each bot separately for two rounds and If he can win both rounds stick with that bot and assume that he is the bot he is fighting.

--- cheating_chester.py
+++ cheating_chester.py
@@ -1,11 +1,16 @@
-# Finds opponent's choice and plays against it. Wins only if it is called second.
-# Doesn't work
+# Cheats, but does not always win. Gets the opponent's play, but cannot guess random numbers.
 
-import rock_paper_scissors
+from rock_paper_scissors import bot_1_name, bot_2_name, player_1_return_plays, player_1_answer, player_2_return_plays, player_2_answer
+
+import importlib
 
 def generate_choice(plays, answer):
-
-    opponents_choice = rock_paper_scissors.bot_1_choice
+    if bot_1_name == "cheating_chester":
+        bot = importlib.import_module(bot_2_name)
+        opponents_choice = bot.generate_choice(player_2_return_plays, player_2_answer)
+    else:
+        bot = importlib.import_module(bot_1_name)
+        opponents_choice = bot.generate_choice(player_1_return_plays, player_1_answer)
 
     if opponents_choice == "ROCK":
         choice = "PAPER"
