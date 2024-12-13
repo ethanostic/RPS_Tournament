86999aa8e95b9f46bc248806268df46a7baee6c3
ethanostic <ethanclausing@gmail.com>
2024-12-13 11:29:50 +0300

    Create template.py
    Created the template file. Shows how to create a bot.

---
+++ template.py
@@ -0,0 +1,8 @@
+# Bot description on how it plays.
+def generate_choice(plays, answer):
+    # Bot Code. Use plays and answers as needed to decide "choice".
+    # The choice should be "ROCK", "PAPER", or "SCISSORS."
+    
+    # For example:
+    choice = "ROCK"
+    return choice
