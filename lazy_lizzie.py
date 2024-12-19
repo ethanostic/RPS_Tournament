1bab1a017495a53b35afabe6c25ed0de6cd6e999
ethanostic <ethanclausing@gmail.com>
2024-12-19 09:16:13 +0300

    December 19, 2024
    None

--- lazy_lizzie.py
+++ lazy_lizzie.py
@@ -1,4 +1,12 @@
-# Always plays rock.
+# Always plays the same play the whole round.
+
+import random
 def generate_choice(plays, answer):
-    choice = "ROCK"
+    if plays:
+        # Repeats first play for the whole round
+        last_round = list(plays.values())[-1]
+        choice = last_round[0]
+    else:
+        # Starts with random play
+        choice = random.choice(["ROCK", "PAPER", "SCISSORS"])
     return choice
